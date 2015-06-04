e83126d4f9ca5fb4587304c037bfbde474277ada
Apocist <apocist@yahoo.com>
2015-06-03 21:20:02 -0700

    Add 'admin' subdomain routable manipulation via admin

--- app/routes/admin.server.routes.js
+++ app/routes/admin.server.routes.js
@@ -1,4 +1,5 @@
 var adminController = require('../controllers/admin.server.controller');
+var routable = require('../controllers/routable.server.controller');
 var express = require('express');
 var vhost = require('vhost');
 
@@ -6,6 +7,12 @@ module.exports = function(app) {
 	var adminRoute = express.Router();
 
 	adminRoute.get('/', adminController.render);
+	adminRoute.route('/routable/:type')
+		.post(routable.create)//works for creating routable in POST
+		.put(routable.update)//works for creating routable in POST
+		.delete(routable.remove);//works for deleting routable in DELETE
+	adminRoute.route('/routable/:type/list').get(routable.listByType);//works for listings objs
+	adminRoute.route('/routable/:type/get/:hid').get(routable.getObjByHid);//works for grabbing certain obj
 
 	app.use(vhost('admin.*.*',adminRoute));//admin.something.com
 
--- app/routes/main.server.routes.js
+++ app/routes/main.server.routes.js
@@ -1,4 +1,3 @@
-var routable = require('../controllers/routable.server.controller');
 var routing = require('../controllers/route.server.controller');
 var express = require('express');
 var vhost = require('vhost');
@@ -7,12 +6,7 @@ module.exports = function(app) {
 	//noinspection JSUnresolvedFunction,JSCheckFunctionSignatures
 	var mainRoute = express.Router();
 
-	mainRoute.route('/routable/:type')
-		.post(routable.create)//works for creating routable in POST
-		.put(routable.update)//works for creating routable in POST
-		.delete(routable.remove);//works for deleting routable in DELETE
-	mainRoute.route('/routable/:type/list').get(routable.listByType);//works for listings objs
-	mainRoute.route('/routable/:type/get/:hid').get(routable.getObjByHid);//works for grabbing certain obj
+
 	mainRoute.route('*')
 		.all(routing.routeByURL)//gets a route by friendly url
 		.all(routing.routeByID);//gets a route by hid
