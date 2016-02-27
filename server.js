90b7ce7b4c0cd297e1bed8588789116f53c34961
Apocist <apocist@yahoo.com>
2016-02-26 22:29:05 -0800

    Testing plugins

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ var 	config = require('./config/config'),
 		mongoose = require('./config/mongoose'),
 		express = require('./config/express');
 
+var PluginManager = require('polite-plugin-manager');
+
+
+/*PluginManager.registerMany(__dirname+'/plugins').start(function() {
+
+	console.log('loaded packages!');
+	//console.log('there are ', PluginManager.packages.length, ' packages')
+});*/
+
 mongoose(function(d){
 	express(function(ap){
 		var db = d,
@@ -14,4 +23,6 @@ mongoose(function(d){
 		console.log(process.env.NODE_ENV  + ' server running at http://localhost:' + config.port);
 	});
 });
-	
+
+
+
