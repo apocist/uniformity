2f7680412d821afa503fd6d10f888a7765291e40
Apocist <apocist@yahoo.com>
2016-03-05 11:03:52 -0800

    update dependencies

--- app/controllers/routable/blog.routable.server.controller.js
+++ app/controllers/routable/blog.routable.server.controller.js
@@ -6,6 +6,6 @@ exports.render = function(req, res, obj, objData) {
 	PermissionController.hasAccess(req.user, objData, [PermissionController.access.readAll], function(bool){
 		if(bool) {
 			res.render('routable/blog', objData);
-		} else {res.render('404', req);;}//TODO need no permission page or something
+		} else {res.render('404', req);}//TODO need no permission page or something
 	});
 };
