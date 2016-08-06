f85802968082acf5b8ad13cdeda6472a013e413e
Apocist <apocist@yahoo.com>
2016-08-06 11:52:25 -0700

    Sub Menu

--- app/views/angular/js/navigationController.js
+++ app/views/angular/js/navigationController.js
@@ -2,13 +2,17 @@ define(['angular', 'underscore'], function(angular) {
 	return angular.module('Element.navigationController', ['ngMaterial', 'ngMessages']).
 	controller('navigationController', function ($scope, $routeParams, APIService, controllerService) {
 		if(!controllerService.navigationController){
-			console.log('Loading Navigation Controller');
 			controllerService.navigationController = {
-				init:true,
+				init:false,
 				APIService: APIService,
-				scopes: [$scope],
+				scopes: [],
 				SiteMap: [],
 				currentMenuItem: {
+					parent: null,
+					children: [],
+					url: null
+				},
+				primaryMenuItem: {
 					parent: null,
 					children: [],
 					url: null
@@ -21,29 +25,52 @@ define(['angular', 'underscore'], function(angular) {
 				},
 				updateScopes: function() {
 					var that = this;
+					that.updatePrimaryMenuItem();
 					_.each(that.scopes, function(scope){
 						scope.SiteMap = that.SiteMap;
 						scope.currentMenuItem = that.currentMenuItem;
+						scope.primaryMenuItem = that.primaryMenuItem;
+						scope.currentNavUrl = that.primaryMenuItem.url || $routeParams.url;
 					});
+				},
+				updatePrimaryMenuItem: function() {
+					var that = this;
+					if(that.currentMenuItem){//was loaded
+						that.getMainParentMenuItem(that.currentMenuItem, function(menuItem){
+							that.primaryMenuItem = menuItem;
+						});
+					}
+
+				},
+				getMainParentMenuItem: function(item, callback){
+					var that = this;
+					if(item._id && item.parent) {//was loaded
+						that.getMainParentMenuItem(
+							that.SiteMap.find(function (menuItem) {
+								return item.parent === menuItem._id;
+							}),
+							callback
+						);
+					} else {
+						callback(item);
+					}
 				},
 				fetchSiteMap: function () {
 					var that = this;
-					this.APIService.getModel('MenuItem').success(function (response) {
+					//that.init = true;
+					that.APIService.getModel('MenuItem').success(function (response) {
 						if (response.success == true && Object.keys(response.data).length) {
 							that.SiteMap = response.data;
+							console.log('SiteMap', that.SiteMap);
 							that.updateCurrentMenuItem();
 						} else {
 							console.error('Cannot retrieve menu!');
 						}
-
 					});
 				}
 			};
-			controllerService.navigationController.fetchSiteMap();
-		} else {
-			console.log('Navigation already loaded');
-			controllerService.navigationController.scopes.push($scope);
 		}
+		controllerService.navigationController.scopes.push($scope);
 
 		$scope.isActiveLink = function (url) {
 			return url === $routeParams.url ? 'active' : '';
@@ -54,7 +81,11 @@ define(['angular', 'underscore'], function(angular) {
 			//console.log('sidebar', ($scope.currentMenuItem && ($scope.currentMenuItem.parent || $scope.currentMenuItem.children.length > 0)) || 'nothing');
 			return (controllerService.navigationController.currentMenuItem && (controllerService.navigationController.currentMenuItem.parent || controllerService.navigationController.currentMenuItem.children.length > 0));
 		};
-		$scope.getCurrentNavLink = $routeParams.url;//static  upon load
+		$scope.currentNavUrl = $routeParams.url;
+
+		if(!controllerService.navigationController.init){
+			controllerService.navigationController.fetchSiteMap();
+		}
 
 	});
 });
--- app/views/angular/js/routeController.js
+++ app/views/angular/js/routeController.js
@@ -2,14 +2,12 @@ define(['angular'], function(angular) {
 	return angular.module('API.routeController', ['Element.navigationController'])./* Route Controller */
 	controller('routeController', function ($scope, $routeParams, APIService, Model, controllerService) {
 		controllerService.routeController = $scope;
-		console.log('Dynamic!!');
-		console.log('$scope', $scope);
 		console.log('$routeParams', $routeParams);
 		APIService.getRoutableByUrl($routeParams.url).success(function (response) {
-			console.log('response', response);
+			//console.log('response', response);
 			if (response.data != null && Object.keys(response.data).length) {
 				$scope.model = new Model(response.data);
-				console.log('name', $scope.model.name);
+				//console.log('name', $scope.model.name);
 				$scope.templateUrl = '/routable/' + $scope.model.__t + '.html';
 			} else if ($routeParams.url == '/' || $routeParams.url == '' || $routeParams.url == null) {
 				$scope.templateUrl = '/site/index.html';
