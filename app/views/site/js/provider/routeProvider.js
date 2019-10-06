49eb95113271e4e46f3ec62c43a37a74f333f778
Apocist <apocist@yahoo.com>
2019-10-05 21:25:35 -0700

    Empty, fresh start!

--- app/views/site/js/provider/routeProvider.js
+++
@@ -1,7 +0,0 @@
-define(['uniformityApp'], function(app) {
-	return app.config(function($routeProvider, $locationProvider) {
-		$routeProvider.		when(":url*", {template: '<div ng-include="templateUrl">Loading...</div>', controller: "routeController"});
-		//otherwise({redirectTo: '/'});
-		$locationProvider.html5Mode(true);
-	});
-});
