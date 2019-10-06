920663850fbb6c63d78ef039d9dd69a40c129708
Apocist <apocist@yahoo.com>
2019-10-05 22:01:01 -0700

    lb4 tests

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {UniformityApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {UniformityApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new UniformityApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
